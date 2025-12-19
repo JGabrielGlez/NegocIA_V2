7956aef14719ae83e0972560ef149a21fb571c57
JGabrielGlez <jegagonzalezes@ittepic.edu.mx>
2025-12-18 21:06:43 -0700

    Creacion de grupos y directorios de la App

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -5,5 +6,6 @@ export default function Index() {
     <View className="flex-1 justify-center items-center">
       <Text className="text-xl text-blue-900">Hola, quier ver asdfasdfcomo se actualiza</Text>
     </View>
+    
   );
 }
