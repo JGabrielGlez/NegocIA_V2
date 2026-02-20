7811dbc0375ac55b70ceb54bc196c568a2ab6696
JGabrielGlez <jegagonzalezes@ittepic.edu.mx>
2026-02-20 11:03:05 -0700

    Configuracion de variables de entorno

--- firebase/databaseService.ts
+++ firebase/databaseService.ts
@@ -1,49 +1,48 @@
-import { addDoc, collection, getDoc, serverTimestamp,doc, query, where, getDocs } from "firebase/firestore";
+import {
+    addDoc,
+    collection,
+    getDocs,
+    query,
+    serverTimestamp,
+    where,
+} from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
-import { ProductoFirestore,COLLECTIONS } from "../store/types";
+import { COLLECTIONS, ProductoFirestore } from "../store/types";
 
 export const databaseServide = {
     // Añadir un producto a la colección, cabe mencionar que el producto ya debe de venir completo desde el momento en el que se manda para agregarlo a la colección.
 
     // Consultar un objeto de la base de datos
-    async consultarProductos(userId:string):Promise <ProductoFirestore[]>{
-        try{
-           const prodRef = collection(db, COLLECTIONS.PRODUCTOS);
-           const q = query(prodRef, where("usuarioId", "==", userId));//filtro de seguridad para solo traerme lo referido a la cuenta de cada usuario. 
+    async consultarProductos(userId: string): Promise<ProductoFirestore[]> {
+        try {
+            const prodRef = collection(db, COLLECTIONS.PRODUCTOS);
+            const q = query(prodRef, where("usuarioId", "==", userId)); //filtro de seguridad para solo traerme lo referido a la cuenta de cada usuario.
 
-        //    Ejecutar el query
-           const querySnapshot = await getDocs(q);
-            
-        //    Mapear los datos obtenidos de la query
-           return querySnapshot.docs.map(producto=>({
-                id:producto.id,
+            //    Ejecutar el query
+            const querySnapshot = await getDocs(q);
+
+            //    Mapear los datos obtenidos de la query
+            return querySnapshot.docs.map((producto) => ({
+                id: producto.id,
                 ...producto.data(),
-           })) as ProductoFirestore[];
-
-        }catch(error:any){
-            console.log(
-                error.code
-            );
-            return[];
-            
+            })) as ProductoFirestore[];
+        } catch (error: any) {
+            console.log(error.code);
+            return [];
         }
-    }
-
-    
-
+    },
 
     // Añadir un objeto a la colecion de producrtos
     async agregarProducto(prod: ProductoFirestore) {
         try {
             // Add a new document in collection "cities"
             // Add a new document with a generated id.
-            await addDoc(collection(db,COLLECTIONS.PRODUCTOS ), {
+            await addDoc(collection(db, COLLECTIONS.PRODUCTOS), {
                 ...prod,
-                fechaAgregado :serverTimestamp(),
+                fechaAgregado: serverTimestamp(),
             });
 
             // TODO al agregar un producto, debo hacer un request a la bd para tener localmeente su id
-
         } catch (error: any) {
             console.log(error.code);
         }
--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -5,19 +5,13 @@ import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCnsLl7HYSii2nPE2nWXcKAZvg9nueJxMM",
-
-    authDomain: "negocia-a17c5.firebaseapp.com",
-
-    projectId: "negocia-a17c5",
-
-    storageBucket: "negocia-a17c5.firebasestorage.app",
-
-    messagingSenderId: "129510647165",
-
-    appId: "1:129510647165:web:a57abb79bd4cc7834a5814",
-
-    measurementId: "G-HN3DZG2NF8",
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inicializa Firebase
@@ -30,7 +24,7 @@ export const functions = getFunctions(app);
 
 // Esto es para usar los emuladores en lugar de los recursos reales
 if (__DEV__) {
-    const ip = "192.168.1.104";
+    const ip = "192.168.1.141";
     // Si usas un celular físico, cambia 'localhost' por la IP de tu PC
     connectAuthEmulator(auth, "http://" + ip + ":1212");
     connectFirestoreEmulator(db, ip, 1214);
