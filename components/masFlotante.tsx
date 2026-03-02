4b5de6facd2306fcee7163e54226937a4dae94ba
JGabrielGlez <jegagonzalezes@ittepic.edu.mx>
2026-03-02 13:49:15 -0700

    style: boton agregar producto mejorado

--- components/masFlotante.tsx
+++ components/masFlotante.tsx
@@ -1,5 +1,5 @@
 import { estilos } from "@/constantes/estilos";
-import { Plus } from "lucide-react-native";
+import { Lock, Plus } from "lucide-react-native";
 import { TouchableOpacity } from "react-native";
 
 type props = {
@@ -11,13 +11,19 @@ export default function BotonMasFlotante({ accion, disabled = false }: props) {
     return (
         <TouchableOpacity
             className={`absolute bottom-16 right-6 h-16 w-16 items-center justify-center rounded-full ${
-                disabled ? "bg-gray-400" : "bg-primary"
+                disabled
+                    ? "bg-slate-200 border-2 border-slate-300"
+                    : "bg-emerald-600"
             }`}
             style={estilos.sombraNormal}
             // Este debe de abrir la ventana modal para añadir los productos
             onPress={disabled ? undefined : accion}
             disabled={disabled}>
-            <Plus size={28} color={disabled ? "#999" : "white"} />
+            {disabled ? (
+                <Lock size={24} color="#94a3b8" />
+            ) : (
+                <Plus size={28} color="white" />
+            )}
         </TouchableOpacity>
     );
 }
